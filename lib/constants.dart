7d336b1262a79b23f1ac5f13aaebe013cc3b1b43
aku <micedreams@gmail.com>
2022-01-05 17:03:31 +0530

    colors

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = const Color(0xff181818);
+Color secondaryColor = const Color(0xFF212121);
+Color accentColor = const Color(0xFF373737);
+Color darkColor = const Color(0xff121212);
+
+const double myPadding = 5.5;
+const iconSize = 27.00;
